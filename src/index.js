14b76a8683d7ee4edd3c9d9b6b94f2b18e03feb2
bassdev <56109283+usamahbass@users.noreply.github.com>
2020-03-05 13:26:35 +0700

    PWA commit index

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import Routes from "./routes/routes";
 import "./index.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
+import * as serviceWorker from "./serviceWorker"
 import "./style/bootstrap.css";
 
 ReactDOM.render(<Routes />, document.getElementById("root"));
